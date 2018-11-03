afeabcaabc8e3da18b6ad35796c381fef31ad57e
Gysco <zackary.beaugelin@epitech.eu>
2018-11-03 10:21:42 +0100

    update parser phantom

--- phantom.py
+++ phantom.py
@@ -1,34 +1,45 @@
-from random import randrange, choice
-from time import sleep
+import sys
 from ast import literal_eval
+from random import choice, randrange
+from time import sleep
+
 
 def lancer():
     sleep(3)
     fini = False
     old_question = ""
     while not fini:
-        infof = open('./1/infos.txt','r')
+        infof = open('./1/infos.txt', 'r')
         lines = infof.readlines()
         infof.close()
         if len(lines) > 0:
             phantom_color = lines[0].split(':')[-1].strip()
             fini = "Score final" in lines[-1]
         # print(phantom_color)
-        qf = open('./1/questions.txt','r')
+        qf = open('./1/questions.txt', 'r')
         question = qf.read()
         qf.close()
-        if question != old_question :
-            rf = open('./1/reponses.txt','w')
-            if ('position' in question.lower()):
-                pos_list = question.split(':')[1].strip()[1:].split('}')[0].strip()
+        if question != old_question:
+            rf = open('./1/reponses.txt', 'w')
+            if ('{' in question.lower()):
+                pos_list = question.split('{')[1].strip().split('}')[0].strip()
                 pos_list = [int(i) for i in pos_list.split(',')]
                 rf.write(str(choice(pos_list)))
-            elif('tuiles' in question.lower()):
-                pos_list = question.split(':')[1].strip()[1:].split(']')[0].strip()
+            elif('[' in question.lower()):
+                pos_list = question.split('[')[1].strip().split(']')[0].strip()
                 pos_list = pos_list.split(',')
                 indices = [i for i, s in enumerate(pos_list) if phantom_color in s.lower()]
-                print(indices)
                 rf.write(str(indices[0]) if len(indices) > 0 else str(randrange(len(pos_list))))
+            elif ('(' in question.lower()):
+                pos_list = question.split('(')[1].strip().split(')')[0].strip()
+                if ('-' in pos_list):
+                    pos_list = pos_list.split('-')
+                elif ('/' in pos_list):
+                    pos_list = pos_list.split('/')
+                else:
+                    print('error parsing: \x1B[3m{:}\x1B[23m ; token not found.'.format(question.lower()), file=sys.stderr)
+                    pos_list = [0, 1]
+                rf.write(str(randrange(int(pos_list[0]), int(pos_list[1]))))
             else:
                 rf.write(str(randrange(6)))
             rf.close()
