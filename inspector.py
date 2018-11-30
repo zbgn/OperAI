19d22cd26393fa9db57075cd0cd09213b0c94f6a
Gysco <zackary.beaugelin@epitech.eu>
2018-11-30 18:01:34 +0100

    Comment for IA

--- inspector.py
+++ inspector.py
@@ -1,20 +1,48 @@
 from random import randrange
 
 def lancer():
+    sleep(3)
     fini = False
     old_question = ""
     while not fini:
-        qf = open('./0/questions.txt','r')
-        question = qf.read()
-        qf.close()
-        if question != old_question :
-            rf = open('./0/reponses.txt','w')
-            rf.write(str(randrange(2)))
-            rf.close()
-            old_question = question
-        infof = open('./0/infos.txt','r')
+        infof = open('./0/infos.txt', 'r')
         lines = infof.readlines()
         infof.close()
         if len(lines) > 0:
+            # phantom_color = lines[0].split(':')[-1].strip()
             fini = "Score final" in lines[-1]
+        # print(phantom_color)
+        qf = open('./0/questions.txt', 'r')
+        question = qf.read()
+        qf.close()
+        if question != old_question:
+            rf = open('./0/reponses.txt', 'w')
+            # Choix des positions (salles)
+            if ('{' in question.lower()):
+                pos_list = question.split('{')[1].strip().split('}')[0].strip()
+                pos_list = [int(i) for i in pos_list.split(',')]
+                rf.write(str(choice(pos_list)))
+            # Choix de la tuile (personnage)
+            elif('[' in question.lower()):
+                pos_list = question.split('[')[1].strip().split(']')[0].strip()
+                pos_list = pos_list.split(',')
+                # util pour le phantom uniquement
+                # indices = [i for i, s in enumerate(pos_list) if phantom_color in s.lower()]
+                # rf.write(str(indices[0]) if len(indices) > 0 else str(randrange(len(pos_list))))
+                rf.write(str(randrange(len(pos_list))))
+            # Parsing pouvoir
+            elif ('(' in question.lower()):
+                pos_list = question.split('(')[1].strip().split(')')[0].strip()
+                if ('-' in pos_list):
+                    pos_list = pos_list.split('-') # Choix des salles pour les pouvoirs
+                elif ('/' in pos_list):
+                    pos_list = pos_list.split('/') # Activation du pouvoir
+                else:
+                    print('error parsing: \x1B[3m{:}\x1B[23m ; token not found.'.format(question.lower()), file=sys.stderr)
+                    pos_list = [0, 1]
+                rf.write(str(randrange(int(pos_list[0]), int(pos_list[1]))))
+            else:
+                rf.write(str(randrange(6)))
+            rf.close()
+            old_question = question
     print("partie finie")
--- phantom.py
+++ phantom.py
@@ -21,21 +21,24 @@ def lancer():
         qf.close()
         if question != old_question:
             rf = open('./1/reponses.txt', 'w')
+            # Choix des positions (salles)
             if ('{' in question.lower()):
                 pos_list = question.split('{')[1].strip().split('}')[0].strip()
                 pos_list = [int(i) for i in pos_list.split(',')]
                 rf.write(str(choice(pos_list)))
+            # Choix de la tuile (personnage)
             elif('[' in question.lower()):
                 pos_list = question.split('[')[1].strip().split(']')[0].strip()
                 pos_list = pos_list.split(',')
                 indices = [i for i, s in enumerate(pos_list) if phantom_color in s.lower()]
                 rf.write(str(indices[0]) if len(indices) > 0 else str(randrange(len(pos_list))))
+            # Parsing pouvoir
             elif ('(' in question.lower()):
                 pos_list = question.split('(')[1].strip().split(')')[0].strip()
                 if ('-' in pos_list):
-                    pos_list = pos_list.split('-')
+                    pos_list = pos_list.split('-') # Choix des salles pour les pouvoirs
                 elif ('/' in pos_list):
-                    pos_list = pos_list.split('/')
+                    pos_list = pos_list.split('/') # Activation du pouvoir
                 else:
                     print('error parsing: \x1B[3m{:}\x1B[23m ; token not found.'.format(question.lower()), file=sys.stderr)
                     pos_list = [0, 1]
