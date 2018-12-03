54a9af9a1345351673ed18fee97162bc72cf3d49
Gysco <zackary.beaugelin@epitech.eu>
2018-12-03 09:59:32 +0100

    Update parsing room - character

--- phantom.py
+++ phantom.py
@@ -5,18 +5,6 @@ from collections import namedtuple
 
 color = ['rose','rouge','gris','bleu','marron','noir','blanc','violet']
 
-#Pouvoir suivant la couleur
-color_power = {
-    'rose' : 'permanents',
-    'rouge' : 'deux',
-    'gris' : 'deux',
-    'bleu' : 'deux',
-    'violet': 'avant',
-    'marron' : 'avant',
-    'noir' : 'apres',
-    'blanc' : 'apres'
-}
-
 status = {
     'suspect' : 0,
     'clean' : 1
@@ -24,13 +12,11 @@ status = {
 
 #Passages entre salles
 debug = True
-salles = [[],[],[],[],[],[],[],[],[],[]]
+salles = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
 
 #passages = [{1,4},{0,2},{1,3},{2,7},{0,5,8},{4,6},{5,7},{3,6,9},{4,9},{7,8}]
 passages = [[1,4],[0,2],[1,3],[2,7],[0,5],[0,8],[5,8],[3,6],[6,9],[3,9],[4,6],[5,7],[4,9],[7,8]]
 pass_ext = [{1,4},{0,2,5,7},{1,3,6},{2,7},{0,5,8,9},{4,6,1,8},{5,7,2,9},{3,6,9,1},{4,9,5},{7,8,4,6}]
-
-Personnage = namedtuple("personnage", "color power salle status indice")
 
 #
 # Groupe suspect par groupe de deux minimum
@@ -62,7 +48,6 @@ def get_closest(rooms):
             #check si on peux faire communiquer deux de ces personnes
             if passage[0] in positions and passage[1] in positions:
                 return 0
-    return 0
     #Sinon pour chaque personne dans la piece si il y a encore des suspects
     for personnage in room:
         if personnage.status == 'suspect':
@@ -79,6 +64,19 @@ def get_emptiest(rooms):
         current += 1
     return rooms[emptiest][0].indice
 
+def update_room(direction, character):
+    c = None
+    print(salles, direction, character)
+    for key, value in salles.items():
+        _ = key
+        for i, v in enumerate(value):
+            if v['color'] == character:
+                c = v
+                del value[i]
+    if c is not None:
+        salles[direction].append(c)
+    print(salles, direction, character)
+
 def send_response(rf, msg):
     msg = str(msg)
     if debug:
@@ -87,8 +85,9 @@ def send_response(rf, msg):
 
 def lancer():
     fini = False
-    old_question = ""
+    old_question = ''
     first = True
+    choosen_char = ''
     while not fini:
         infof = open('./1/infos.txt', 'r')
         lines = infof.readlines()
@@ -96,25 +95,19 @@ def lancer():
         if len(lines) > 0:
             #Parsing des personnages
             if first:
-                personnages = [item.strip() for item in lines[3].split('  ')]
-                for personnage in personnages:
+                for personnage in [item.strip() for item in lines[3].split()]:
                     pos_col, pos_salle, pos_status = personnage.split('-')
-                    pos_salle = int(pos_salle)
-                    pos_power = color_power[pos_col]
-                    perso = Personnage(color=pos_col, power=pos_power, salle=pos_salle, status=pos_status, indice=0)
-                    salles[pos_salle].append(personnage)
-                phantom_color = lines[0].split(':')[-1].strip()
-                #error here
-                phantom_power = color_power[phantom_color]
+                    salles[int(pos_salle)].append({'color':pos_col, 'status':pos_status})
+                # phantom_color = lines[0].split(':')[-1].strip()
             fini = "Score final" in lines[-1] and not first
         first = False
         #print(phantom_color)
-        #pintt(phantom_power)
+        #print(phantom_power)
         qf = open('./1/questions.txt', 'r')
         question = qf.read()
         qf.close()
 
-        if question != old_question and question != "":
+        if question != old_question and question != '':
             if debug:
                 print (question)
 
@@ -122,34 +115,27 @@ def lancer():
 
             # Choix des positions (salles)
             if ('{' in question.lower()):
-                #on choisit une possiton               
+                #on choisit une positon               
                 pos_list = question.split('{')[1].strip().split('}')[0].strip()
                 pos_list = [int(i) for i in pos_list.split(',')]
                 
-                send_response(rf, choice(pos_list))
+                direction = choice(pos_list)
+                update_room(direction, choosen_char)
+                send_response(rf, direction)
 
             # Choix de la tuile (personnage)
             elif('[' in question.lower()):
                 # on rempli les salles
                 pos_list = question.split('[')[1].strip().split(']')[0].strip()
                 pos_list = pos_list.split(',')
-                indices = [i for i, s in enumerate(pos_list) if phantom_color in s.lower()]
-                
-                indice = 0
-                new_salles = [[],[],[],[],[],[],[],[],[],[]]
-                for pos in pos_list:
-                    pos_col, pos_salle, pos_status = pos.strip().split('-')
-                    pos_salle = int(pos_salle)
-                    pos_power = color_power[pos_col]
-                    personnage = Personnage(color=pos_col, power=pos_power, salle=pos_salle, status=pos_status, indice=indice)
-                    new_salles[pos_salle].append(personnage)
-                    indice += 1
-                
-                chosen_room = 0
-                #chosen_room = get_fullest(salles)
-                #chosen_room = get_emptiest(salles)
-                #chosen_room = get_closest(salles)
-                send_response(rf, chosen_room)
+                # indices = [i for i, s in enumerate(pos_list) if phantom_color in s.lower()]
+           
+                choosen_room = 0
+                choosen_char = pos_list[choosen_room].split('-')[0] #get color of choosen 
+                #choosen_room = get_fullest(salles)
+                #choosen_room = get_emptiest(salles)
+                #choosen_room = get_closest(salles)
+                send_response(rf, choosen_room)
                 
             # Parsing pouvoir
             elif ('(' in question.lower()):
